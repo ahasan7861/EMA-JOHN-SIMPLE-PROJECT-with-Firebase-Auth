68276821105306ceedd0f3f82318fe2b72efb790
ahasan7861 <ahfahasan786@gmail.com>
2023-03-28 16:33:44 +0600

    Ningth Update

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -19,14 +19,17 @@ const Shop = () => {
     useEffect(()=>{
         
         const storedCart = getShoppingCart();
+        const savedCart = [];
         for(const id in storedCart){
             const addedProduct = products.find(product => product.id === id)
             
-
-            const quantity = storedCart[id];
-            addedProduct.quantity = quantity
-            console.log(addedProduct)
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }           
         }
+        setCart(savedCart);
     }, [products])
 
 
